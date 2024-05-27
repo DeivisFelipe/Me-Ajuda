bab4ffabdc5f6c051028693187f45a9e7894056e
DeivisFelipe <guerreirodosul01@gmail.com>
2024-05-26 21:54:07 -0300

    feat: mudando o favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Favicons -->
+        <link rel="icon" type="image/x-icon" sizes="180x180" href="icon_MeAjuda.png">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
